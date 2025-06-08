52ee466fb3d456fc16cb331f19b452371ed0c94a
miesnerjacob <miesner.jacob@gmail.com>
2025-06-08 15:53:34 -0400

    completed exercise 1.7

--- part-1/exercises/unicafe/src/App.jsx
+++ part-1/exercises/unicafe/src/App.jsx
@@ -8,16 +8,59 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Statistics = ({ text, count }) => <p>{text} {count}</p>
+const Statistics = ({ text, stat }) => <p>{text} {stat}</p>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  }
+
+  const calculateAverage = ({ good, neutral, bad }) => {
+    const goods = Array(good).fill(1)
+    const neutrals = Array(neutral).fill(0)
+    const bads = Array(bad).fill(-1)
+
+    const nums = [...goods, ...neutrals, ...bads]
+
+    if (nums.length === 0) {
+      return 'No ratings submmitted yet...'
+    }
+
+    const numsSum = nums.reduce((acc, val) => acc + val, 0)
+    const numsLength = nums.length
+    return numsSum / numsLength
+  }
+
+  const calculatePctPositive = ({ good, neutral, bad }) => {
+    const totalVotes = (good + neutral + bad)
+    if (totalVotes === 0) {
+      return 'No ratings submmitted yet...'
+    }
+
+    return (good / totalVotes * 100) + '%'
+  }
+
+  let average = calculateAverage({ good, neutral, bad })
+  let pctPositive = calculatePctPositive({ good, neutral, bad })
 
   return (
     <div>
@@ -26,9 +69,12 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='neutral'/>
       <Button onClick={handleBadClick} text='bad'/>
       <h1>statistics</h1>
-      <Statistics text='good' count={good}/>
-      <Statistics text='neutral' count={neutral}/>
-      <Statistics text='bad' count={bad}/>
+      <Statistics text='good' stat={good}/>
+      <Statistics text='neutral' stat={neutral}/>
+      <Statistics text='bad' stat={bad}/>
+      <Statistics text='total' stat={total}/>
+      <Statistics text='average' stat={average}/>
+      <Statistics text='positive' stat={pctPositive}/>
     </div>
   )
 }
