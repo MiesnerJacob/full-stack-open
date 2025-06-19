3e4112b5e921d079afa88d6359483eb7e861bbba
miesnerjacob <miesner.jacob@gmail.com>
2025-06-19 12:49:29 -0400

    finished 2.9

--- part2/exercises/phonebook/src/App.jsx
+++ part2/exercises/phonebook/src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
@@ -29,16 +31,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={newFilter} onChange={handleFilterChange}/></div>
+      <h2>Add a new</h2>
       <form>
         <div> name: <input value={newName} onChange={handleNameChange}/></div>
         <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div><button type="submit" onClick={addName}>add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personsToShow.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
